54b29da315fbb0843f206b56d903f145520a3771
IsaMaqueda <isamqd@gmail.com>
2021-10-30 12:03:17 -0500

    funciones Isa

--- functions/index.js
+++ functions/index.js
@@ -3,11 +3,19 @@ const functions = require('firebase-functions');
 
 // The Firebase Admin SDK to access Firestore.
 const admin = require('firebase-admin');
+
 admin.initializeApp();
 
 const db = admin.firestore();
 
+//modulo express para que este script pueda responder cosas a los usuarios
+const express = require('express')
+const app= express()
 
+//funcion para comprobar la conexion
+app.get('/hello-world', (req, res)=>{
+    return res.status(200).json({message:'Hello World!'})
+});
 /* 
 Check and retrieve data on a specific user using their Email as their ID
  Example user below
@@ -99,11 +107,109 @@ exports.getOrdersByPharmacy = functions.https.onRequest(async(req, res)=>{
     res.json(data);
 });
 
+
+//creates the order
+exports.createOrder = functions.https.onRequest(async (req, res)=>{
+
+    /*
+    "id": 1,
+		"user": 10,
+		"pharmacy": 1,
+		"order_datetime": "2021-10-26T16:26:00",
+		"sent_datetime": "",
+		"status": "waiting",
+		"content": ["Tylenol"],
+		"total": 12.5
+    */
+    const id = Number(req.query.id);
+    const user = Number(req.query.user);
+    const pharmacy = Number(req.query.pharmacy);
+    const order_datetime =new Date();
+    const sent_datetime=new Date(order_datetime.getTime() + 7200000)
+    const status=req.query.status;
+    const content= req.query.content;
+    const total = req.query.total;
+
+    //conecta con la coleccion 'order'
+    const userRef = db.collection('order');
+    //busca si existe algun doc con el id  recibido
+    const queryRefU = await userRef.where('id', '==', id).get();
+
+    if (queryRefU.empty){
+        const writeResult = await userRef.doc().set({
+            id: id,
+            user:user,
+            pharmacy:pharmacy,
+            order_datetime:order_datetime,
+            sent_datetime:sent_datetime,
+            status:status,
+            content:content,
+            total:total
+            });
+        res.set('Access-Control-Allow-Origin', '*');
+        return res.json({result: `Order con ID: ${id} ha sido añadido.`});
+    }
+    else{
+        //si existe un doc con el id recibido devuelve un mensaje de error
+        res.set('Access-Control-Allow-Origin', '*');
+        return res.json({result: `El id de la orden  ya fue registrado`});
+    }
+});
+
+exports.updateOrder = functions.https.onRequest(async(req, res)=>{
+    //recibe la order id 
+    const id = Number(req.query.id);
+
+    //conecta con la coleccion 
+    const userRef = db.collection('order');
+    //busca el doc que coincide con el libro_id
+    const snapshot = await userRef.where('id', '==', id).get()
+
+    if (snapshot.empty) {
+        //si ningun libro coincide manda mensaje de error
+        res.set('Access-Control-Allow-Origin', '*');
+        return res.json({result: `Ningúna orden coincide con el ID.`});
+      }
+        snapshot.forEach(doc => {
+        //modifica el status de la orden segun el status recibido
+        doc.ref.update({
+            status: "sent",
+            sent_datetime=new Date()
+        });
+      });
+      //devuelve mensaje de exito
+      res.set('Access-Control-Allow-Origin', '*');
+      return res.json({result: `El id ha sido modificado.`});
+
+});
+
+
+
+exports.getProductsByWarehouse = functions.https.onRequest(async(req, res)=>{
+    
+    const warehouseID = req.query.warehouseID;
+    const orderRef = db.collection('orders');
+
+    const snapshot = await orderRef.where('warehouses', '==', parseInt(warehouseID)).get();
+    if(snapshot.empty){
+        res.json({res: 'could not find any products for warehouse ' + warehouseID});
+        return
+    }
+    //create data set to return
+    data = {}
+    snapshot.forEach(doc =>{
+        data[doc.id] = doc.data();
+    })
+    res.json(data);
+});
+
+exports.app=functions.https.onRequest(app);
+
 // getUser <-- done 
 // updateUser() <-- done
 // getOrdersByPharmacy() <-- done
 // getProductsByPhramacy(area)
 // getProductsByWarehouse()
-// createOrder()
-// updateOrder() //change order status
+// createOrder() <-- done 
+// updateOrder() //change order status <-- done 
 // requestRestock(pharmacia, productos, cantidades)
