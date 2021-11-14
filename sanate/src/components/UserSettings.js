b66676deaf484e376967c1f78de95d97af5e9eb5
Roberto Soto <A01023599@itesm.mx>
2021-11-14 12:47:43 -0600

    Started work on screen to allow user to change their settings

--- sanate/src/components/UserSettings.js
+++ sanate/src/components/UserSettings.js
@@ -1,9 +1,14 @@
+import { useState } from 'react';
+
 const UserSettings = ({user}) => {
+	const [showPrefs, setShowPrefs] = useState(false);
+
 	return (
 		<div className="user-settings">
-		<h5>
+		<h5 onClick={() => setShowPrefs(!showPrefs)}>
 			{"Welcome, "+user.name+"!"}
 		</h5>
+		{showPrefs? <>HELLO</> : <></>}
 		</div>
 	)
 }
